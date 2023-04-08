7dcf3bc1361bf8e35a8b288822511df764981ba6
Mike Reiche <mike.reiche@ewerk.com>
2023-04-08 14:29:24 +0200

    Separated mdc integration and added icon button

--- src/my-app.ts
+++ src/my-app.ts
@@ -4,6 +4,8 @@ import "@material/web/switch/switch"
 import "@material/web/checkbox/checkbox"
 import "@material/web/segmentedbuttonset/outlined-segmented-button-set"
 import "@material/web/segmentedbutton/outlined-segmented-button"
+import "@material/web/divider/divider"
+import "@material/web/iconbutton/standard-icon-button"
 
 export class MyApp {
   public message = 'Hello World!'
