9cbc1de838e8f8559ef585e0a580a9e1f93408ae
Mike Reiche <mike@reiche.world>
2026-08-06 16:47:33 +0200

    chore: Update to Aurelia 2.0.0-rc.2, aurelia-mdui 0.0.3 and mreiche-aurelia-components 2.0.3 (#3)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import {Aurelia, Registration} from 'aurelia'
 import {MyApp} from './my-app'
 import {MduiWebTask} from "aurelia-mdui"
-import {NumberValueConverter} from "t-systems-aurelia-components/src/value-converters/number-value-converter";
+import {NumberValueConverter} from "mreiche-aurelia-components/src/value-converters/number-value-converter";
 import {ISanitizer} from "@aurelia/runtime-html"
-import {SanitizeHtmlHtmlSanitizer} from "t-systems-aurelia-components/src/value-converters/sanitize-html-html-sanitizer";
-import {PercentValueConverter} from "t-systems-aurelia-components/src/value-converters/percent-value-converter";
+import {SanitizeHtmlHtmlSanitizer} from "mreiche-aurelia-components/src/value-converters/sanitize-html-html-sanitizer";
+import {PercentValueConverter} from "mreiche-aurelia-components/src/value-converters/percent-value-converter";
 import 'mdui/mdui.css'
 import 'mdui'
 
